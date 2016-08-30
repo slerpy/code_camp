125bebf9f28bba66821fb20e015a7775dc57d238
slerpy <slerpy@riseup.net>
2016-08-30 04:44:29 -0700

    objectifying

---
+++ music_objects.js
@@ -0,0 +1,49 @@
+
+
+var collection = {
+    "2548": {
+        "album": "Pretty Hate Machine",
+        "artist": "Nine Inch Nails",
+        "tracks": [
+            "Head Like A Hole",
+            "Terrible Lie"
+        ]
+    },
+    "2468": {
+        "album": "Dilate",
+        "artist": "Ani DiFranco",
+        "tracks": [
+            "Napolean",
+            "Dilate"
+        ]
+    },
+    "1245": {
+        "artist": "Purity Ring",
+        "tracks": [ ]
+    },
+    "5439": {
+        "album": "Mezzanine"
+    }
+};
+// Keep a copy of the collection for testing
+var collectionCopy = JSON.parse(JSON.stringify(collection));
+
+function updateRecords(id, prop, value) {
+    if (value !== "") {
+        if (prop !== "tracks") {
+            collection[id][prop] = value;
+        } else {
+            if (collection[id].tracks) {
+                collection[id].tracks.push(value);
+            } else {
+                collection[id].tracks = [];
+                collection[id].tracks.push(value);
+            }
+        }
+    } else {
+        delete collection[id][prop];
+    }
+    return collection;
+}
+// Test
+updateRecords(5439, "artist", "Massive Attack");
